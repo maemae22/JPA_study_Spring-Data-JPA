d2da44568ce5f731fd971aac59fd80546926a9a9
maemae22 <maemae2@naver.com>
2023-04-19 19:30:45 +0900

    Web 확장 - 페이징과 정렬
    - page index는 0번부터 하는걸로 놔두는 것이 제일 깔끔하다
    - 꼭 DTO로 변환해서 반환하기

--- src/main/java/study/datajpa/controller/MemberController.java
+++ src/main/java/study/datajpa/controller/MemberController.java
@@ -1,9 +1,13 @@
 package study.datajpa.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import study.datajpa.dto.MemberDTO;
 import study.datajpa.entity.Member;
 import study.datajpa.repository.MemberRepository;
 
@@ -26,8 +30,19 @@ public class MemberController {
         return member.getUsername();
     }
 
+    // http://localhost:8080/members?page=0&size=3&sort=id,desc&sort=username,desc
+    @GetMapping("/members")
+    public Page<MemberDTO> list(@PageableDefault(size = 5, sort = "username") Pageable pageable) {
+        Page<Member> page = memberRepository.findAll(pageable);
+//        Page<MemberDTO> toMap = page.map(m -> new MemberDTO(m.getId(), m.getUsername(), null));
+        Page<MemberDTO> toMap = page.map(MemberDTO::new);
+        return toMap;
+    }
+
     @PostConstruct
     public void init() {
-        memberRepository.save(new Member("userA"));
+        for (int i=0; i<100; i++) {
+            memberRepository.save(new Member("user"+i, i));
+        }
     }
 }
